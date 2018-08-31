0f80b8157fc94fa06ff67ff0a35e46f5b1a028e3
Administrator <ghjg@ttk.hh>
2018-08-31 11:33:16 +0800

    默认发送短信成功

--- com.passport.service/src/main/java/com/passport/service/impl/ProxyInfoServiceImpl.java
+++ com.passport.service/src/main/java/com/passport/service/impl/ProxyInfoServiceImpl.java
@@ -1,9 +1,9 @@
 package com.passport.service.impl;
 
-import com.alibaba.dubbo.config.annotation.Service;
 import com.common.mongo.AbstractMongoService;
 import com.passport.domain.ProxyInfo;
 import com.passport.service.ProxyInfoService;
+import org.springframework.stereotype.Service;
 
 @Service
 public class ProxyInfoServiceImpl extends AbstractMongoService<ProxyInfo> implements ProxyInfoService {
