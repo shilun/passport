54f21d70c93bc77637e4e31b19a44b4eb9a73bb8
lunsh <martin.shi@nidone.com.cn>
2018-08-31 13:25:18 +0800

    添加代理商信息

--- com.passport.main/ui/src/app/routes/system/user-list/user-list.component.ts
+++ com.passport.main/ui/src/app/routes/system/user-list/user-list.component.ts
@@ -2,20 +2,25 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {AbstractController} from '../../../common/abstract.controller';
 import {UserService} from '../../../services/user.service';
+import {GlobalService} from "../../../services/global.service";
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html'
 })
 export class UserListComponent extends AbstractController implements OnInit {
-
-  constructor(protected userService: UserService, protected route: ActivatedRoute, protected router: Router) {
+  statuses:any;
+  constructor(protected userService: UserService,protected globalService: GlobalService,  protected route: ActivatedRoute, protected router: Router) {
     super(userService, route, router);
-    this.entity = {id: '', pin: '', nickName: '', phone: '', email: '', sexType: '', status: ''};
+    this.entity = {};
   }
 
-  ngOnInit() {
+  async ngOnInit() {
     this.list(this.entity);
+    let result = await this.globalService.list('yesorno');
+    if (result.success) {
+      this.statuses = result.data.list;
+    }
   }
 
 }
