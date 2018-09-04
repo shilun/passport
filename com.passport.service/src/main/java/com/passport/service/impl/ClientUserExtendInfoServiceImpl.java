ca5c15b5c74d90a57115b23a1d99faf80135088c
Administrator <ghjg@ttk.hh>
2018-09-04 09:20:18 +0800

    添加service

--- com.passport.service/src/main/java/com/passport/service/impl/ClientUserExtendInfoServiceImpl.java
+++ com.passport.service/src/main/java/com/passport/service/impl/ClientUserExtendInfoServiceImpl.java
@@ -3,11 +3,13 @@ package com.passport.service.impl;
 import com.common.mongo.AbstractMongoService;
 import com.passport.domain.ClientUserExtendInfo;
 import com.passport.service.ClientUserExtendInfoService;
+import org.springframework.stereotype.Service;
 
 /**
  * @author Luo
  * @date 2018/9/3 19:44
  */
+@Service
 public class ClientUserExtendInfoServiceImpl extends AbstractMongoService<ClientUserExtendInfo> implements ClientUserExtendInfoService {
     @Override
     protected Class getEntityClass() {
