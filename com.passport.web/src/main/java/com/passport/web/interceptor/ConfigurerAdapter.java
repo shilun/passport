eb6904ee7e17c8384b39edb61d03e375b7d63dee
lunsh <lunshi@tt.com>
2018-09-29 11:45:01 +0800

    重置接口

--- com.passport.web/src/main/java/com/passport/web/interceptor/ConfigurerAdapter.java
+++ com.passport.web/src/main/java/com/passport/web/interceptor/ConfigurerAdapter.java
@@ -1,6 +1,7 @@
 package com.passport.web.interceptor;
 
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -12,7 +13,17 @@ public class ConfigurerAdapter extends WebMvcConfigurerAdapter {
     private UserSessionHandlerInterceptor resourceHandlerInterceptor;
 
     @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
+
+    @Override
     public void addInterceptors(InterceptorRegistry registry) {
+
        registry.addInterceptor(resourceHandlerInterceptor).addPathPatterns("/**").excludePathPatterns(new String[]{"/error",
                "/login/*",
                "/appinterface/user-login",
