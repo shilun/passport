c706e9c13d3b6f58d77545de4df7cd6888e1a6db
lunsh <lunshi@tt.com>
2019-01-01 15:46:22 +0800

    修改代理商

--- com.passport.web/src/main/java/com/passport/web/controller/ConfigController.java
+++ com.passport.web/src/main/java/com/passport/web/controller/ConfigController.java
@@ -1,18 +1,12 @@
 package com.passport.web.controller;
 
-import com.passport.domain.AppConfig;
 import com.passport.service.AppConfigService;
 import com.passport.web.AbstractClientController;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.text.MessageFormat;
-import java.util.concurrent.TimeUnit;
 
 @RestController
 public class ConfigController extends AbstractClientController {
@@ -21,8 +15,7 @@ public class ConfigController extends AbstractClientController {
     private AppConfigService appConfigService;
 
 
-    @RequestMapping(value = "appConfig", method = {RequestMethod.GET})
-    @ResponseBody
+    @RequestMapping(value = "/appConfig", method = {RequestMethod.GET})
     public String AppConfig() {
         Long id = getDomain().getId();
         return appConfigService.findByProxyId(id);
