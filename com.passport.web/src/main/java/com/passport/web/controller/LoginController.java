2a97678696221a973c78ef00befc5588ba50ab60
lunsh <lunshi@tt.com>
2019-01-03 10:22:13 +0800

    修改软件url

--- com.passport.web/src/main/java/com/passport/web/controller/LoginController.java
+++ com.passport.web/src/main/java/com/passport/web/controller/LoginController.java
@@ -256,7 +256,7 @@ public class LoginController extends AbstractClientController {
         String domain = StringUtils.getDomain(getRequest().getRequestURL().toString());
         String[] domains = getDomain().getDomain();
         if (domains.length > 2 && domains[0].equals(domain)) {
-            return "redirect:https://passport." + domains[1] + "/login/reg?q=" + q;
+            return "redirect:http://passport." + domains[1] + "/login/reg?q=" + q;
         }
         AgentTypeEnum agentType = getAgentType();
         if (agentType == AgentTypeEnum.Android || agentType == AgentTypeEnum.Other) {
