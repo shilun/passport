9f5172ac32071f292e559984fb3b5f0c6851e442
lunsh <lunshi@tt.com>
2019-01-09 16:18:03 +0800

    change msg

--- com.passport.web/src/main/resources/static/js/register.js
+++ com.passport.web/src/main/resources/static/js/register.js
@@ -180,7 +180,7 @@ $(document).ready(function(){
 function isWeiXin() {
     var ua = window.navigator.userAgent.toLowerCase();
     console.log(ua);//mozilla/5.0 (iphone; cpu iphone os 9_1 like mac os x) applewebkit/601.1.46 (khtml, like gecko)version/9.0 mobile/13b143 safari/601.1
-    if (ua.match(/MicroMessenger/i) == 'micromessenger') {
+    if (ua.indexOf("micromessenger") != -1) {
         return true;
     } else {
         return false;
