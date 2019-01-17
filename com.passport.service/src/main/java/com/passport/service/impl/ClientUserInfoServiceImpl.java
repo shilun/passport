382baf85c837f98e16904bcc1660f9144123d31b
lunsh <lunshi@tt.com>
2019-01-17 10:02:39 +0800

    change

--- com.passport.service/src/main/java/com/passport/service/impl/ClientUserInfoServiceImpl.java
+++ com.passport.service/src/main/java/com/passport/service/impl/ClientUserInfoServiceImpl.java
@@ -316,6 +316,10 @@ public class ClientUserInfoServiceImpl extends AbstractMongoService<ClientUserIn
                 String oldTokenKey = o1.toString();
                 redisTemplate.delete(oldTokenKey);
             }
+            //默认用户无该数据则设置为 不是推广人
+            if(userInfo.getPopularize()==null){
+                userInfo.setPopularize(YesOrNoEnum.NO.getValue());
+            }
             dto = new UserDTO();
             BeanCoper.copyProperties(dto, userInfo);
 
