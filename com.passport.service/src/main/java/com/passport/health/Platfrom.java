6b55a99829c6b883bf82d989021251001d5a92dc
lz <782168307@qq.com>
2019-02-18 15:41:18 +0800

    rpc接口监控

---
+++ com.passport.service/src/main/java/com/passport/health/Platfrom.java
@@ -0,0 +1,20 @@
+package com.passport.health;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.common.health.AbstractHealThIndicator;
+import com.common.rpc.StatusRpcService;
+import com.passport.rpc.UserRPCService;
+import com.platform.rpc.RecommendRPCService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Platfrom extends AbstractHealThIndicator {
+
+    @Reference
+    private RecommendRPCService recommendRPCService;
+
+    @Override
+    protected StatusRpcService getStatusRpcService() {
+        return recommendRPCService;
+    }
+}
