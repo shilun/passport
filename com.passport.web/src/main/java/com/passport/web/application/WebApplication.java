cf381e7312fe5a85c1cd1498274bf5283df7bf23
lunsh <tt@tt.com>
2019-09-08 15:47:01 +0800

    init

--- com.passport.web/src/main/java/com/passport/web/application/WebApplication.java
+++ com.passport.web/src/main/java/com/passport/web/application/WebApplication.java
@@ -1,38 +1,20 @@
 package com.passport.web.application;
 
-import com.common.application.AbstractApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+
 @ComponentScan(basePackages = {"com.passport", "com.common.config"})
-public class WebApplication extends AbstractApplication {
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, MongoAutoConfiguration.class})
+public class WebApplication {
     private static Logger logger = LoggerFactory.getLogger("starting");
 
     public static void main(String[] args) {
-        WebApplication instance = WebApplication.getInstance(WebApplication.class);
-        SpringApplication app = new SpringApplication(WebApplication.class);
-        app.addListeners(instance.buildListeners());
-        app.run();
+        SpringApplication.run(WebApplication.class);
     }
-
-    @Override
-    protected void starting() {
-        logger.info("web.starting----------------");
-    }
-
-    @Override
-    protected void startFailed(Throwable throwable) {
-        logger.error("web.error", throwable);
-    }
-
-    @Override
-    protected void started() {
-        logger.info("web.started");
-    }
-
 }
