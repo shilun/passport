06ac2f6d9eedd7f3e64c0f099c6379e10f02c1ef
lunsh <tt@tt.com>
2019-09-10 09:58:34 +0800

    init

--- com.passport.main/src/main/java/com/passport/main/application/MainApplication.java
+++ com.passport.main/src/main/java/com/passport/main/application/MainApplication.java
@@ -4,12 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages = {"com.passport", "com.common.config"})
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class,MongoAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class MainApplication {
     private static Logger logger = LoggerFactory.getLogger("starting");
 
